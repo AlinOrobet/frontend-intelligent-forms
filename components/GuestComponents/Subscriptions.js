cee5660d886e14896fe7d5b172345eee795e9c10
FlorinCamarut1 <florin.camarut@student.usv.ro>
2023-03-10 17:12:30 +0200

    inceput subs

--- components/GuestComponents/Subscriptions.js
+++ components/GuestComponents/Subscriptions.js
@@ -1,7 +1,22 @@
 import React from "react";
-
+import Image from "next/image";
 function Subscriptions() {
-  return <div>Subscriptions</div>;
+  return (
+    <div className="max-w-[1200px] m-auto h-full w-full p-5 md:p-7 lg:px-10 text-white font-secondary font-bold ">
+      <h2 className="text-2xl">Subscriptions and contact</h2>
+      <div>
+        <div className="bg-primary text-secondary">
+          <div className="relative w-8 h-8">
+            <Image src="/assets/main/free-sub.png" fill />
+          </div>
+          <h1> Free subscription</h1>
+          <p> you can have a maximum of 3 forms at the same time</p>
+          <p>Price: free</p>
+          <button>GET NOW</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Subscriptions;
