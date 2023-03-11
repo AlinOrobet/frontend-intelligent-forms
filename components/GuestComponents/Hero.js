e5424215c3e3ed6615015551e57844a2f5b9a6fd
FlorinCamarut1 <florin.camarut@student.usv.ro>
2023-03-11 21:29:45 +0200

    fixed an issue with overflowing on screen mode

--- components/GuestComponents/Hero.js
+++ components/GuestComponents/Hero.js
@@ -8,7 +8,7 @@ function Hero() {
       className="h-screen w-screen p-5 md:p-7 lg:px-10 font-secondary font-bold text-primary relative lg:overflow-hidden "
       id="hero"
     >
-      <h2 className=" text-4xl md:text-5xl  ">
+      <h2 className=" text-4xl md:text-5xl    ">
         Your solution for reducing bureaucracy and simplifying your daily tasks
       </h2>
 
@@ -18,18 +18,18 @@ function Hero() {
 
         {/* We understand part */}
 
-        <p className=" text-2xl md:text-3xl text-center lg:text-left  row-start-2 md:row-start-1 lg:flex lg:items-center    ">
+        <p className=" text-2xl md:text-3xl text-center lg:text-left  row-start-2 md:row-start-1 lg:flex lg:items-end    ">
           We understand how frustrating it can be to navigate through endless
           paperwork and bureaucratic red tape, which is why we're here to help.
         </p>
 
         {/* GET STARTED button*/}
-        <button className="  text-secondary font-secondary bg-primary py-2 lg:py-1 mt-5 rounded-md  text-2xl  shadow-2xl row-start-3 md:row-start-2  md:w-[200px]  md:m-auto lg:ml-0   transition ease-in-out delay-150  hover:scale-110 hover:bg-slate-300  duration-500 ">
+        <button className="  text-secondary font-secondary bg-primary py-2 lg:py-1 mt-5 rounded-md  text-2xl  shadow-2xl row-start-3 md:row-start-2  md:w-[200px]  md:m-auto lg:ml-0 lg:mt-5   transition ease-in-out delay-150  hover:scale-110 hover:bg-slate-300  duration-500 ">
           <Link href="/" />
           GET STARTED
         </button>
 
-        <div className="relative w-[340px] h-[220px] md:h-[500px] md:w-[700px] lg:w-[800px] lg:h-[550px] row-start-1 md:row-start-3 lg:row-start-1 lg:col-span-2 lg:row-span-2 m-auto   ">
+        <div className="relative w-[340px] h-[220px] md:h-[500px] md:w-[700px] lg:w-[800px] lg:h-[540px] xl:w-[900px] xl:h-[600px] row-start-1 md:row-start-3 lg:row-start-1 lg:col-span-2 lg:row-span-2 m-auto  ">
           <Image src="/assets/main/hero.png" alt="imagine-hero" fill />
         </div>
       </div>
