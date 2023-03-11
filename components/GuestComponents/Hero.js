a51218cfc77aa0563b7b96d00b76d34a39916472
FlorinCamarut1 <95957482+FlorinCamarut1@users.noreply.github.com>
2023-03-11 07:45:36 +0200

    started simplifying code for hero section

--- components/GuestComponents/Hero.js
+++ components/GuestComponents/Hero.js
@@ -4,34 +4,36 @@ import Link from "next/link";
 
 function Hero() {
   return (
-    <div className="h-screen w-full p-5 md:p-7 lg:px-10 font-secondary font-bold text-white   ">
-      <h2 className=" text-3xl  md:text-4xl lg:text-5xl  ">
+    <section
+      className="h-screen w-screen p-5 md:p-7 lg:px-10 font-secondary font-bold text-primary "
+      id="#hero"
+    >
+      <h2 className=" text-4xl  md:text-4xl lg:text-5xl  ">
         Your solution for reducing bureaucracy and simplifying your daily tasks
       </h2>
 
       {/* imag + inner block */}
-      <div className=" flex flex-col-reverse  items-center justify-center">
+      <div className="grid gap-10 justify-center pt-5">
         {/* WE understand + button inner block */}
-        <div className=" space-y-10 md:pr-8 ">
-          {/* We understand part */}
 
-          <p className=" text-xl text-center md:text-left lg:text-2xl lg:mr-[50px]">
-            We understand how frustrating it can be to navigate through endless
-            paperwork and bureaucratic red tape, which is why we're here to
-            help.
-          </p>
+        {/* We understand part */}
 
-          {/* GET STARTED */}
-          <button className=" text-secondary bg-white py-3 px-[90px]  rounded-md font-bold text-lg block m-auto shadow-2xl md:m-0 md:px-[60px] lg:py-1 hover:scale-110 active:scale-90 ">
-            <Link href="/" />
-            GET STARTED
-          </button>
-        </div>
-        <div className="relative w-[380px] h-[260px] ">
+        <p className=" text-2xl text-center md:text-left lg:text-2xl lg:mr-[50px] row-start-2">
+          We understand how frustrating it can be to navigate through endless
+          paperwork and bureaucratic red tape, which is why we're here to help.
+        </p>
+
+        {/* GET STARTED button*/}
+        <button className=" row-start-3 text-secondary bg-primary py-3 mt-5 rounded-md  text-xl  shadow-2xl  ">
+          <Link href="/" />
+          GET STARTED
+        </button>
+
+        <div className="relative w-[340px] h-[220px] row-start-1 ">
           <Image src="/assets/main/hero.png" alt="imagine-hero" fill />
         </div>
       </div>
-    </div>
+    </section>
   );
 }
 
